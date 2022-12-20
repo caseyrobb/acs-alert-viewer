f57ba8df14da884306cdaf80f566efd2d7712367
Casey Robb <casey.robb@redhat.com>
2022-12-20 10:56:30 -0700

    Added cache, pagination and foundation css

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import redis
 from redis.commands.json.path import Path
 from flask import Flask, request, Response, jsonify, json, abort, render_template
+from flask_caching import Cache
+from flask_paginate import Pagination, get_page_args
 from datetime import datetime
 
 app = Flask(__name__)
+cache = Cache(
+    app, config={"CACHE_TYPE": "redis", "CACHE_REDIS_URL": "redis://redis:6379/0"}
+)
 
 # redis connection
 r = redis.StrictRedis(host="redis", port=6379, decode_responses=True)
@@ -22,7 +27,7 @@ def post_alert():
     return Response(status=200)
 
 
-@app.template_filter('formatDate')
+@app.template_filter("formatDate")
 def formatDate(sdate):
     return datetime.strptime(sdate[:19], "%Y-%m-%dT%H:%M:%S")
 
@@ -34,7 +39,9 @@ def get_alert(id):
         return jsonify(id=f"{id}", status="Not found")
     return alert
 
+
 # @app.route('/api/v1/alerts', methods=['GET'])
+@cache.cached(timeout=20)
 def get_alerts():
     alerts = []
     keys = r.keys("alert:*")
@@ -43,9 +50,28 @@ def get_alerts():
     return jsonify(alerts)
 
 
+def get_alerts_page(alerts, offset=0, per_page=10):
+    return alerts[offset : offset + per_page]
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", alerts=get_alerts().json)
+    page, per_page, offset = get_page_args(
+        page_parameter="page", per_page_parameter="per_page"
+    )
+    alerts = get_alerts().json
+    total = len(alerts)
+    pagination_alerts = get_alerts_page(alerts, offset=offset, per_page=per_page)
+    pagination = Pagination(
+        page=page, per_page=per_page, total=total, css_framework="foundation"
+    )
+    return render_template(
+        "index.html",
+        alerts=pagination_alerts,
+        page=page,
+        per_page=per_page,
+        pagination=pagination,
+    )
 
 
 @app.route("/alert/<alertId>")
